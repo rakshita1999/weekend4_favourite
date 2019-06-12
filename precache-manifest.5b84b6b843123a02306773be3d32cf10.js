123481b8266f28902c323289ba253ffbe7f6afa7
santoshi rakshita <rakshita1999>
2019-06-12 12:09:26 +0530

    Updates

---
+++ precache-manifest.5b84b6b843123a02306773be3d32cf10.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e64554350b5103c1ad89e2e7854c609",
+    "url": "/weekend4_favourite/index.html"
+  },
+  {
+    "revision": "0f1ba7933943112c964a",
+    "url": "/weekend4_favourite/static/css/main.a2d5f2b3.chunk.css"
+  },
+  {
+    "revision": "006c1e6da0ae3ba6f4b4",
+    "url": "/weekend4_favourite/static/js/2.08ca1eb0.chunk.js"
+  },
+  {
+    "revision": "0f1ba7933943112c964a",
+    "url": "/weekend4_favourite/static/js/main.8cdca3ad.chunk.js"
+  },
+  {
+    "revision": "13ff7c3b4909a7c444eb",
+    "url": "/weekend4_favourite/static/js/runtime~main.47751208.js"
+  }
+]);
